bd4e2aa1b5b7658b420ad140f99dbef0aebb76d4
Kevintravis <kevintravis120@gmail.com>
2020-06-21 19:29:24 +0200

    first commit for api integration

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const path = require('path');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/', express.static(path.resolve(__dirname, './dist')));
+
+app.get('*', function(req, res) {
+    res.sendFile(path.resolve(__dirname, './dist/index.html'));
+    res.end();
+});
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+});
+
+
+// chargebee routes
+const chargebeeRouter = require('./api/routes/RouteModel');
+
+app.use('/chargebee', chargebeeRouter);
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
